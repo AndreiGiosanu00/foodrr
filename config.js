639b245bc5d0fbcdcea53b1904af7a74fb365f0d
agiosanu <agiosanu@lenovo.com>
2021-08-04 19:51:42 +0300

    Add Login with Google.

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC0Q_FGOegySljKxuH5q-dji2yvZCd4fC4",
+    authDomain: "foodrr-4b163.firebaseapp.com",
+    projectId: "foodrr-4b163",
+    storageBucket: "foodrr-4b163.appspot.com",
+    messagingSenderId: "74495830395",
+    appId: "1:74495830395:web:32ce9df19d846391eef5de",
+    measurementId: "G-H3VQ7VQ1HF",
+    databaseURL: "https://foodrr-4b163.firebaseio.com/"
+};
---
+++ screens/LoadingScreen.js
@@ -0,0 +1,39 @@
+import React, {Component} from "react";
+import {View, Text, StyleSheet, ActivityIndicator} from "react-native";
+import firebase from "firebase";
+
+class LoadingScreen extends Component {
+
+    componentDidMount() {
+        this.checkedIfLoggedIn();
+    }
+
+    checkedIfLoggedIn = () => {
+        firebase.auth().onAuthStateChanged(user => {
+           if (user) {
+               this.props.navigation.navigate('DashboardScreen');
+           } else {
+               this.props.navigation.navigate('LoginScreen');
+           }
+        });
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <ActivityIndicator size="large"/>
+            </View>
+        )
+    }
+}
+
+export default LoadingScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
