9661d4c03ab3c29a34c00c0451e6b582d2a8e4ba
agiosanu <agiosanu@lenovo.com>
2021-08-05 15:09:06 +0300

    Add login animation.

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,11 +1,117 @@
 import React, {Component} from "react";
-import {View, Text, StyleSheet, Button, Image, Dimensions} from "react-native";
+import {View, Text, StyleSheet, Dimensions, TextInput} from "react-native";
+import Svg, { Image, Circle, ClipPath } from 'react-native-svg';
 import * as Google from 'expo-google-app-auth';
 import firebase from "firebase";
+import Animated, {Easing} from "react-native-reanimated";
+import {TapGestureHandler, State} from "react-native-gesture-handler";
 
 const {width, height} = Dimensions.get('window');
+const {
+    Value,
+    event,
+    block,
+    cond,
+    eq,
+    set,
+    Clock,
+    startClock,
+    stopClock,
+    debug,
+    timing,
+    clockRunning,
+    interpolate,
+    Extrapolate,
+    concat
+} = Animated;
+
+function runTiming(clock, value, dest) {
+    const state = {
+        finished: new Value(0),
+        position: new Value(0),
+        time: new Value(0),
+        frameTime: new Value(0)
+    };
+
+    const config = {
+        duration: 1000,
+        toValue: new Value(0),
+        easing: Easing.inOut(Easing.ease)
+    };
+
+    return block([
+        cond(clockRunning(clock), 0, [
+            set(state.finished, 0),
+            set(state.time, 0),
+            set(state.position, value),
+            set(state.frameTime, 0),
+            set(config.toValue, dest),
+            startClock(clock)
+        ]),
+        timing(clock, state, config),
+        cond(state.finished, debug('stop clock', stopClock(clock))),
+        state.position
+    ]);
+}
 
 class LoginScreen extends Component {
+
+    constructor() {
+        super();
+
+        this.buttonOpacity = new Value(1);
+        this.onStateChange = event([
+            {
+                nativeEvent: ({state}) => block([
+                    cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 1, 0)))
+                ])
+            }
+        ]);
+
+        this.onCloseState = event([
+            {
+                nativeEvent: ({state}) => block([
+                    cond(eq(state, State.END), set(this.buttonOpacity, runTiming(new Clock(), 0, 1)))
+                ])
+            }
+        ]);
+
+        this.buttonY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [100, 0],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.bgY = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [-height / 3 - 30, 0],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.textInputZIndex = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [1, -1],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.textInputOpacity = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [1, 0],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.textInputY =  interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [0, 100],
+            extrapolate:Extrapolate.CLAMP
+        });
+
+        this.rotateCross = interpolate(this.buttonOpacity, {
+            inputRange: [0, 1],
+            outputRange: [180, 360],
+            extrapolate:Extrapolate.CLAMP
+        });
+    }
 
     isUserEqual = (googleUser, firebaseUser) => {
         if (firebaseUser) {
@@ -87,19 +193,52 @@ class LoginScreen extends Component {
                 backgroundColor: 'white',
                 justifyContent: 'flex-end'
             }}>
-                <View style={{...StyleSheet.absoluteFill}}>
-                    <Image
-                        source={require('../assets/login-background.jpg')}
-                        style={{flex: 1, height: null, width: null}}
-                    />
-                </View>
-                <View style={{height: height / 3, alignItems: 'center'}}>
-                    <View style={styles.button}>
-                        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Sign In</Text>
-                    </View>
-                    <View style={{...styles.button, backgroundColor: '#4285F4'}}>
-                        <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}} onPress={() => this.signInWithGoogleAsync()}>Sign in with Google</Text>
-                    </View>
+                <Animated.View style={{...StyleSheet.absoluteFill, transform: [{translateY: this.bgY}]}}>
+                    <Svg height={height + 60} width={width}>
+                        <ClipPath id="clip">
+                            <Circle r={height + 60} cx={width / 2}/>
+                        </ClipPath>
+                        <Image
+                            href={require('../assets/login-background.jpg')}
+                            width={width}
+                            height={height + 60}
+                            preserveAspectRatio="xMidYMid slice"
+                            clipPath="url(#clip)"
+                        />
+                    </Svg>
+                </Animated.View>
+                <View style={{height: height / 3, justifyContent: 'center'}}>
+                    <TapGestureHandler onHandlerStateChange={this.onStateChange}>
+                        <Animated.View style={{...styles.button, opacity: this.buttonOpacity, transform: [{translateY: this.buttonY}] }}>
+                            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Register</Text>
+                        </Animated.View>
+                    </TapGestureHandler>
+                    <Animated.View style={{...styles.button, backgroundColor: '#4285F4', opacity: this.buttonOpacity, transform: [{translateY: this.buttonY}]}}>
+                        <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}} onPress={() => this.signInWithGoogleAsync()}>Login with Google</Text>
+                    </Animated.View>
+                    <Animated.View style={{height: height / 3, ...StyleSheet.absoluteFill, top: null,
+                        justifyContent: 'center', zIndex: this.textInputZIndex,
+                        opacity: this.textInputOpacity, transform: [{translateY: this.textInputY}]}}
+                    >
+                        <TapGestureHandler onHandlerStateChange={this.onCloseState}>
+                            <Animated.View style={styles.closeButton}>
+                                <Animated.Text style={{fontSize: 15, fontWeight: 'bold', transform: [{rotate: concat(this.rotateCross, 'deg')}]}}>X</Animated.Text>
+                            </Animated.View>
+                        </TapGestureHandler>
+                        <TextInput
+                            placeholder="Email"
+                            style={styles.textInput}
+                            placeholderTextColor="black"
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            style={styles.textInput}
+                            placeholderTextColor="black"
+                        />
+                        <Animated.View style={styles.button}>
+                            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Sign In</Text>
+                        </Animated.View>
+                    </Animated.View>
                 </View>
             </View>
         )
@@ -118,13 +257,38 @@ const styles = StyleSheet.create({
     button: {
         backgroundColor: '#fff',
         height: 70,
-        width: 350,
         marginHorizontal: 20,
         marginVertical: 5,
         borderRadius: 35,
-        paddingLeft: 5,
-        paddingRight: 5,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        shadowOffset: {width: 2, height: 2},
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        elevation: 3
+    },
+    textInput: {
+        height: 50,
+        borderRadius: 25,
+        borderWidth: 1,
+        marginHorizontal: 20,
+        paddingLeft: 10,
+        marginVertical: 5,
+        borderColor: 'rgba(0,0,0,0.2)'
+    },
+    closeButton: {
+        height: 40,
+        width: 40,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: -20,
+        left: width / 2 - 20,
+        shadowOffset: {width: 2, height: 2},
+        shadowColor: 'black',
+        shadowOpacity: 0.2,
+        elevation: 3
     }
 });
