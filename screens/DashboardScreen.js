a48afcae9b01fe0c5af0b88b3a730bd948a37005
agiosanu <agiosanu@lenovo.com>
2021-08-26 11:23:41 +0300

    Add modal for more details in dashboard view.

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {View, Text, StyleSheet, Button, Dimensions, SafeAreaView, Image} from "react-native";
+import {View, Text, StyleSheet, Button, Dimensions, SafeAreaView, Image, Pressable, Modal} from "react-native";
 import { withNavigation } from '@react-navigation/compat';
 import LottieView from "lottie-react-native";
 import {TouchableRipple} from "react-native-paper";
@@ -13,9 +13,16 @@ class DashboardScreen extends Component {
         super();
         this.state = {
             loading: false,
-            noHistory: false
+            noHistory: false,
+            modalShow: false
         }
     }
+
+    changeModalState(value) {
+        console.log('da');
+        this.setState({modalShow: value});
+    }
+
     render() {
         if (this.state.noHistory) {
             return (
@@ -45,13 +52,39 @@ class DashboardScreen extends Component {
                             Hi!
                         </Text>
                     </View>
-
                 </SafeAreaView>
             );
         }
 
         return (
             <SafeAreaView style={styles.container}>
+                {/*More details Modal*/}
+                <Modal animationType="slide"
+                       transparent={true}
+                       visible={this.state.modalShow}>
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalTitle}>Pizza</Text>
+                            <Text style={styles.modalText}>Below you will find more details about your food that was scanned using our app</Text>
+                            <TouchableRipple
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => this.changeModalState(false)}
+                            >
+                                <Icon name="close" color="white" size={15}/>
+                            </TouchableRipple>
+                            <View style={styles.modalContent}>
+                                <Text style={{color: '#777777', fontWeight: 'bold'}}>Date: <Text style={{fontWeight: 'normal'}}>26/08/2021</Text></Text>
+                                <View style={styles.nutrientsTable}>
+                                    <Text style={{color: '#777777', fontWeight: 'bold'}}>Nutrients</Text>
+                                </View>
+                                <Text style={{color: '#777777'}}>This dish goes well with a Diet Coke and some Rice on the side.</Text>
+                                <ScrollView>
+                                    <Text style={{color: '#777777', fontWeight: 'bold'}}>Restaurants in your area that serve this food:</Text>
+                                </ScrollView>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
                 <View style={styles.title}>
                     <Text style={{fontWeight: 'bold', fontSize: 25}}>Welcome to Foodrr</Text>
                     <Text style={{fontSize: 15, color: '#777777'}}>Below you can find your latest scans</Text>
@@ -72,7 +105,7 @@ class DashboardScreen extends Component {
                                     <Icon name="calendar" color="#4285F4" size={25} style={{marginRight: 10, marginLeft: 50}}/>
                                     <Text style={{color: '#777777', fontSize: 15}}>25/08/2021</Text>
                                 </View>
-                                <TouchableRipple onPress={() => {}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
+                                <TouchableRipple onPress={() => {this.changeModalState(true);}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
                                     <View style={{flexDirection: 'row'}}>
                                         <Icon name="unfold-more-vertical" color="#4285F4" size={25}/>
                                         <Text style={{color: '#4285F4', fontWeight: 'bold', marginLeft: 10, fontSize: 15}}>More details</Text>
@@ -93,7 +126,7 @@ class DashboardScreen extends Component {
                                     <Icon name="calendar" color="#4285F4" size={25} style={{marginRight: 10, marginLeft: 50}}/>
                                     <Text style={{color: '#777777', fontSize: 15}}>25/08/2021</Text>
                                 </View>
-                                <TouchableRipple onPress={() => {}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
+                                <TouchableRipple onPress={() => {this.changeModalState(true);}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
                                     <View style={{flexDirection: 'row'}}>
                                         <Icon name="unfold-more-vertical" color="#4285F4" size={25}/>
                                         <Text style={{color: '#4285F4', fontWeight: 'bold', marginLeft: 10, fontSize: 15}}>More details</Text>
@@ -117,7 +150,7 @@ class DashboardScreen extends Component {
                                     <Icon name="calendar" color="#4285F4" size={25} style={{marginRight: 10, marginLeft: 50}}/>
                                     <Text style={{color: '#777777', fontSize: 15}}>25/08/2021</Text>
                                 </View>
-                                <TouchableRipple onPress={() => {}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
+                                <TouchableRipple onPress={() => {this.changeModalState(true);}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
                                     <View style={{flexDirection: 'row'}}>
                                         <Icon name="unfold-more-vertical" color="#4285F4" size={25}/>
                                         <Text style={{color: '#4285F4', fontWeight: 'bold', marginLeft: 10, fontSize: 15}}>More details</Text>
@@ -141,7 +174,7 @@ class DashboardScreen extends Component {
                                     <Icon name="calendar" color="#4285F4" size={25} style={{marginRight: 10, marginLeft: 50}}/>
                                     <Text style={{color: '#777777', fontSize: 15}}>25/08/2021</Text>
                                 </View>
-                                <TouchableRipple onPress={() => {}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
+                                <TouchableRipple onPress={() => {this.changeModalState(true);}} style={{padding: 5, marginTop: 5, marginLeft: 45}}>
                                     <View style={{flexDirection: 'row'}}>
                                         <Icon name="unfold-more-vertical" color="#4285F4" size={25}/>
                                         <Text style={{color: '#4285F4', fontWeight: 'bold', marginLeft: 10, fontSize: 15}}>More details</Text>
@@ -194,5 +227,66 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         borderBottomColor: '#dddddd',
         borderRadius: 30
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        width: width
+    },
+    modalView: {
+        margin: 10,
+        width: width - 20,
+        height: height - 150,
+        backgroundColor: "white",
+        borderRadius: 20,
+        paddingTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        paddingBottom: 20,
+        alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5
+    },
+    button: {
+        borderRadius: 20,
+        padding: 5,
+        elevation: 2,
+        position: 'absolute',
+        top: 7,
+        left: 357
+    },
+    buttonOpen: {
+        backgroundColor: "#F194FF",
+    },
+    buttonClose: {
+        backgroundColor: "#4285F4",
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalTitle: {
+      fontSize: 25,
+      fontWeight: 'bold'
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 15,
+        color: '#777777'
+    },
+    modalContent: {
+
+    },
+    nutrientsTable: {
+
     }
 });
