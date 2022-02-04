9c5a3715023ce23ab344a5c3bf2b7fb6da4fb6ca
Andrei_Giosanu <andrei_giosanu@dellteam.com>
2022-02-04 14:47:41 +0200

    add stuff.

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyC0Q_FGOegySljKxuH5q-dji2yvZCd4fC4",
+    apiKey: "YOUR-API-KEY",
     authDomain: "foodrr-4b163.firebaseapp.com",
     databaseURL: "https://foodrr-4b163.firebaseio.com/",
     projectId: "foodrr-4b163",
     storageBucket: "foodrr-4b163.appspot.com",
     messagingSenderId: "74495830395",
-    appId: "1:74495830395:web:c99cc2f26a354b19eef5de",
-    measurementId: "G-GWYBN17L29"
+    appId: "YOUR-APP-ID",
+    measurementId: "YOU-MEASUREMENT-ID"
 };
